8dc603c1dc8015a27f00f2c4bbd0a7a0e507c477
Rhaliot <filipelliotkozuchowski@proton.me>
2025-11-16 21:10:34 +0100

    list item click behavior

--- src/api.js
+++ src/api.js
@@ -1,15 +1,19 @@
-
-
 export async function fetchRecipes(ingridient) {
-    try {
-        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingridient}`);
-        if (!res.ok) { throw new Error("Web error")};
-        const data = await res.json();
-        console.log(data.meals);
-        return data.meals;
-         {}
-    } catch (err) {
-        console.log(err);
-        return [];
+  try {
+    const res = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingridient}`
+    );
+    if (!res.ok) {
+      throw new Error("Web error");
     }
+    const data = await res.json();
+    console.log(data.meals);
+    return data.meals;
+    {
+    }
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
 }
+
--- src/main.js
+++ src/main.js
@@ -1,26 +1,33 @@
-import {fetchRecipes} from './api.js'
-
-const form = document.getElementById("search")
+import { fetchRecipes } from "./api.js";
+//selectors
+const form = document.getElementById("search");
 const searchInput = document.getElementById("searchInput");
 const recipeList = document.getElementById("recipeList");
+const recipe = document.querySelector('li')
 
+// search logic and showing entries
 form.addEventListener("submit", async (e) => {
-  e.preventDefault(); 
+  e.preventDefault();
 
   const query = searchInput.value.trim();
-  if (!query) return; 
-  recipeList.innerHTML = '';
-  const recipes = await fetchRecipes(query); 
-  
-  recipes.forEach(recipe => {
-  const recipeObject = document.createElement("li")
-  const recipeThumb = document.createElement("img")
-  recipeThumb.src = recipe.strMealThumb;
-  recipeObject.append(recipe.strMeal)
-  recipeList.append(recipeObject);
-  recipeList.append(recipeThumb)
+  if (!query) return;
+  recipeList.innerHTML = "";
+  const recipes = await fetchRecipes(query);
+
+  recipes.forEach((recipe) => {
+    
+    const recipeObject = document.createElement("li");
+    const recipeThumb = document.createElement("img");
+    recipeThumb.src = recipe.strMealThumb;
+    recipeObject.append(recipe.strMeal);
+    recipeList.append(recipeObject);
+    recipeList.append(recipeThumb);
+
+    // list item click behavior
+
+    recipeObject.addEventListener('click', async (e) => {
+        console.log(recipe.idMeal)
+    })
+  });
 });
 
-
-
-});
